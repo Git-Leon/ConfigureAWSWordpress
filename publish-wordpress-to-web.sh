557b77ff34bb1383e9f03ac10f6cda8da888ddf0
Leon Hunter <leonhunter@Leons-MacBook-Pro-2.local>
2019-06-12 15:57:03 -0400

    udpated permissions

--- publish-wordpress-to-web.sh
+++ publish-wordpress-to-web.sh
@@ -51,10 +51,10 @@ changeWordpressEnvironmentPermissions() {
 printNextStep() {
   echo View the WordPress website from within the AWS Cloud9 IDE.
   echo To do this, on the main menu bar, choose Preview, Preview Running Application.
-  echo A new window opens in the IDE and displays a Not Found page (this is expected at this point).
+  echo A new window opens in the IDE and displays a Not Found page. This is expected at this point.
   echo Open the WordPress website in a new tab within the same web browser as the AWS Cloud9 IDE.
   echo To do this, on the address bar in the new window, choose Pop Out Into New Window.
-  echo The new tab displays the same Not Found page (which is still expected at this point).
+  echo The new tab displays the same Not Found page, which is still expected at this point.
   echo In the new tab within the same web browser as the AWS Cloud9 IDE, add /index.php to the end of the existing URL, and then press Enter.
   echo The WordPress websites home page is displayed
 }
