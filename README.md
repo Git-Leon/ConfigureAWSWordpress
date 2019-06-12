c14d4c10e7445fd93813743199df8a375579e394
Leon Hunter <leonhunter@Leons-MacBook-Pro-2.local>
2019-06-12 16:20:30 -0400

    update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Execution order
+* ATTENTION: These scripts must be executed from `~/environment` directory.
 1. `download-wordpress-and-setup-mysql.sh`
 2. `publish-wordpress-to-web.sh`
 3. `enable-incoming-traffic.sh`
--- download-wordpress-and-setup-mysql.sh
+++ download-wordpress-and-setup-mysql.sh
@@ -47,16 +47,31 @@ setUpMySQL() {
 	sudo mysql_secure_installation
 }
 
+printHowToCreateDatabase() {
+ echo Create a MySQL database for the WordPress site to use.
+ echo To do this, run the following command, replacing \"my_db_name\" with a name for the new database, for example, \"mysite\".
+ echo Be sure to save this database name in a secure location for later use.
+ echo CREATE DATABASE \'my_db_name\';
+}
+
+printHowToCreateDatabaseUser() {
+	echo Create a MySQL user for the WordPress site to use.
+	echo To do this, run the following command, replacing \"my_user_name\" with the users name and replacing \"my_password\" with a password for the user
+	echo GRANT ALL PRIVILEGES ON *.* TO \'my_user_name\'@\'localhost\' IDENTIFIED BY \'my_password\';
+}
+
 startMySQLAsRoot() {
+	printHowToCreateDatabase
+	printHowToCreateDatabaseUser
 	sudo mysql -uroot -p
 }
 
 printNextStep() {
-	echo Configure the `wp-config.php` file for the WordPress website before continuing.
-	echo To do this, double-click the `wp-config.php` file to open it in the editor, replace the following values, and then save and close the file.
-  echo Replace `database_name_here` with the name of the MySQL database that you created earlier.
-  echo Replace `username_here` with the name of the MySQL user that you created earlier.
-  echo Replace `password_here` with the password for the MySQL user that you created earlier.
+	echo Configure the \`wp-config.php\` file for the WordPress website before continuing.
+	echo To do this, double-click the \`wp-config.php\` file to open it in the editor, replace the following values, and then save and close the file.
+	echo Replace \`database_name_here\` with the name of the MySQL database that you created earlier.
+	echo Replace \`username_here\` with the name of the MySQL user that you created earlier.
+	echo Replace \`password_here\` with the password for the MySQL user that you created earlier.
 }
 
 
