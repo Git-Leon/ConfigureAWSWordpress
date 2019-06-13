e3c7a325706b76e6b4ce35db2046da907c6aca60
EC2 Default User <ec2-user@ip-172-31-4-65.us-east-2.compute.internal>
2019-06-13 13:03:45 +0000

    update

--- download-wordpress-and-setup-mysql.sh
+++ download-wordpress-and-setup-mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#source ~/environment/aws-wordpress-installation/import_utils.sh
-source ./import_utils.sh
+source ~/environment/aws-wordpress-installation/import_utils.sh
+#source ./import_utils.sh
 
 updateSecurity() {
   sudo yum -y update
